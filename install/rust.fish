618b74bb5c96892c22880a5d0fc3adc3a32f349f
Jonah Snider <jonah@jonah.pw>
2020-10-13 10:04:21 -0700

    fix(rust): install GCC for cargo and Nim
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- install/rust.fish
+++ install/rust.fish
@@ -1,6 +1,9 @@
 #!/usr/bin/env fish
 # Install Rust and tools via cargo
 
+# Install GCC for cargo (and also secretly Nim)
+sudo apt install -yqq build-essential
+
 # Download installer
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup.sh
 sh /tmp/rustup.sh -yq --no-modify-path
