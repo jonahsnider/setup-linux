47259e10e2af7d65e0a2d7ee08398a75d4b82eb3
Jonah Snider <jonah@jonah.pw>
2020-10-13 10:39:31 -0700

    fix(node): fix `node_version` variable usage

--- install/node.fish
+++ install/node.fish
@@ -4,7 +4,7 @@
 set node_version 14
 
 # Install Node.js
-curl -sL "https://deb.nodesource.com/setup_"node_version".x" | sudo -E bash -
+curl -sL "https://deb.nodesource.com/setup_"$node_version".x" | sudo -E bash -
 sudo apt-get install -yqq nodejs
 
 # Install Yarn package manager
