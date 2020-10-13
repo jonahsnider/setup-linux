437096d9a7ef43c5f3c71130fd4a175d4506cf59
Jonah Snider <jonah@jonah.pw>
2020-10-13 08:53:13 -0700

    fix(node): use `-y` flag to install Yarn

--- install/node.fish
+++ install/node.fish
@@ -12,4 +12,4 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt update
-sudo apt install yarn
+sudo apt install -y yarn
