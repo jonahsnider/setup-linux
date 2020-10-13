c557a993fb43bd9598884ed7575726322d2dda48
Jonah Snider <jonah@jonah.pw>
2020-10-13 09:50:19 -0700

    fix: fix path of fish config and change shell to fish
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- install/fish-config.fish
+++ install/fish-config.fish
@@ -1,4 +1,6 @@
 #!/usr/bin/env fish
 
 # Make fish's config file execute the one in here
-echo source $SETUP_LINUX_INSTALL_DIR/../config/fish/config.fish > ~/.config/fish/config.fish
+echo source $SETUP_LINUX_INSTALL_DIR/config/fish/config.fish > ~/.config/fish/config.fish
+
+sudo chsh -s (which fish)
