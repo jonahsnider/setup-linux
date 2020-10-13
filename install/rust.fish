f355af51f1dd4296cfeae7a5aae5b9cea946b91e
Jonah Snider <jonah@jonah.pw>
2020-10-13 10:39:51 -0700

    fix(rust): update env for current shell after installing
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- install/rust.fish
+++ install/rust.fish
@@ -9,6 +9,9 @@ sudo apt install -yqq build-essential libssl-dev pkg-config
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup.sh
 sh /tmp/rustup.sh -yq --no-modify-path
 
+# Only needed to update this instance of the shell with cargo binaries
+source $HOME/.cargo/env
+
 # Starship
 cargo install starship
 ln -s $SETUP_LINUX_INSTALL_DIR/config/starship/starship.toml ~/.config/starship.toml
