9ebbdf7b5ff716919ba9c3b179b84617844b9468
Jonah Snider <jonah@jonah.pw>
2020-10-13 10:27:10 -0700

    fix(rust): install other build tools for Linux
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- install/rust.fish
+++ install/rust.fish
@@ -1,8 +1,9 @@
 #!/usr/bin/env fish
 # Install Rust and tools via cargo
 
-# Install GCC for cargo (and also secretly Nim)
-sudo apt install -yqq build-essential
+# Install build tools
+# Namely GCC for cargo (and also secretly Nim)
+sudo apt install -yqq build-essential libssl-dev pkg-config
 
 # Download installer
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup.sh
