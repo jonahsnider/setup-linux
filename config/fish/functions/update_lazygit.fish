a99f841599e07cbdf61bdfa95e5a67213eeb11d4
Jonah Snider <jonah@jonah.pw>
2020-10-13 16:47:37 -0700

    feat: add script to update lazygit

---
+++ config/fish/functions/update_lazygit.fish
@@ -0,0 +1,13 @@
+function update_lazygit -d "Update lazygit to the specified version"
+  set installDir /tmp
+  set filename $installDir/lazygit_$argv
+
+  # Download
+  wget -q -O $filename.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v"$argv"/lazygit_"$argv"_Linux_x86_64.tar.gz"
+  # Download
+  tar xf $filename.tar.gz -C $installDir
+  # Replace version
+  mv $installDir/lazygit (which lazygit)
+
+  echo lazygit $argv installed
+end
