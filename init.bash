6b88d01d17ccce0cda1a5bf6b0b3063d5dac42d7
Jonah Snider <jonah@jonah.pw>
2020-10-13 09:03:47 -0700

    fix: make apt commands quieter
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- init.bash
+++ init.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-add-repository -y ppa:fish-shell/release-3
-sudo apt-get update
-sudo apt-get install fish -y
+sudo apt-get update -qq
+sudo apt-get install fish -yqq
 
 fish ./install/entry.fish
 
--- install/node.fish
+++ install/node.fish
@@ -5,11 +5,11 @@ set node_version 14
 
 # Install Node.js
 curl -sL "https://deb.nodesource.com/setup_"node_version".x" | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -yqq nodejs
 
 # Install Yarn package manager
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt update
-sudo apt install -y yarn
+sudo apt install -yqq yarn
