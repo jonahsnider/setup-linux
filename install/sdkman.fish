6d260bdf2cf7261a0c180a3098b787d912964c04
Jonah Snider <jonah@jonah.pw>
2020-10-17 19:22:09 -0700

    fix(sdkman): fix SDKMAN! erroneously saying it isn't installed
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- install/sdkman.fish
+++ install/sdkman.fish
@@ -2,5 +2,10 @@
 # Install SDKMAN! integration for fish
 
 fisher add reitzig/sdkman-for-fish@v1.4.0
-sdk install java
-sdk install gradle
+
+
+echo "if you are prompted to install SDKMAN! again just ignore that"
+echo "you need to open a new instance of fish to have the `sdk` function fully installed"
+echo "check this file for the commands to install the tools with SDKMAN!"
+source "sdk install java"
+source "sdk install gradle"
