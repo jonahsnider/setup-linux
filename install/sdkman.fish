3ae19e9c8d8103a210a4d1da6cd784fec37ede70
Jonah Snider <jonah@jonah.pw>
2020-10-18 04:38:02 -0700

    remove extra newline

--- install/sdkman.fish
+++ install/sdkman.fish
@@ -3,7 +3,6 @@
 
 fisher add reitzig/sdkman-for-fish@v1.4.0
 
-
 echo "if you are prompted to install SDKMAN! again just ignore that"
 echo "you need to open a new instance of fish to have the `sdk` function fully installed"
 echo "check this file for the commands to install the tools with SDKMAN!"
