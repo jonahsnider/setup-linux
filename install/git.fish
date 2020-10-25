6e43a998d7d573719d15c345a969738374484144
Jonah Snider <jonah@jonah.pw>
2020-10-24 17:01:26 -0700

    feat(git): install GCM
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- install/git.fish
+++ install/git.fish
@@ -3,3 +3,9 @@
 
 ln -s $SETUP_LINUX_INSTALL_DIR/config/git/.gitconfig ~/.gitconfig
 pass init E3D78B8F4596CD635F304439E85CE1D319132409
+
+set filename /tmp/gcmcore-linux_amd64.deb
+# Once GCM goes stable this should be removed
+wget -q -O $filename https://github.com/microsoft/Git-Credential-Manager-Core/releases/download/v2.0.252-beta/gcmcore-linux_amd64.2.0.252.766.deb
+sudo apt install -yqq $filename
+git-credential-manager-core configure
