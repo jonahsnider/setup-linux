153b5c87c214ae696c69c4b340ae63a5c319b86c
Jonah Snider <jonah@jonah.pw>
2021-01-04 09:37:32 -0800

    fix(keybase): fix Keybase installation

--- install/keybase.fish
+++ install/keybase.fish
@@ -4,12 +4,14 @@
 set filename /tmp/keybase_amd64.deb
 
 wget -q -O $filename https://prerelease.keybase.io/keybase_amd64.deb
-sudo apt install -yqq filename
+sudo apt install -yqq $filename
 
 echo "you will be asked to sign in to Keybase and register this device on the sigchain"
 echo "since my Linux instances are highly ephermeral the names should be unique and follow a sequential pattern"
 echo "if this is the home PC, the name should be `WSL Home PC XXX`, where XXX is the number of this Linux install"
 run_keybase
+
+keybase login
 
 keybase pgp export | gpg --import
 keybase pgp export -s | gpg --allow-secret-key-import --import
