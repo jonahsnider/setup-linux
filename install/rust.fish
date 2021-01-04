fcf9566dacbba2b13811e97b7ec2d5e646a21fab
Jonah Snider <jonah@jonah.pw>
2021-01-04 09:11:55 -0800

    fix(rust): fix ~/.cargo/env no longer being fish compatible

--- install/rust.fish
+++ install/rust.fish
@@ -5,9 +5,6 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup.sh
 sh /tmp/rustup.sh -yq --no-modify-path
 
-# Only needed to update this instance of the shell with cargo binaries
-source $HOME/.cargo/env
-
 # Starship
-cargo install starship
+~/.cargo/bin/cargo install starship
 ln -s $SETUP_LINUX_INSTALL_DIR/config/starship/starship.toml ~/.config/starship.toml
