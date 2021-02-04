e8325956305173592f4a7dba96e6ae841e33dd6e
Jonah Snider <jonah@jonah.pw>
2021-02-03 23:11:53 -0800

    feat(node): install Yarn via npm

--- install/node.fish
+++ install/node.fish
@@ -8,11 +8,7 @@ fnm install $node_version
 fnm use $node_version
 
 # Install Yarn package manager
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-sudo apt update -qq
-sudo apt install -yqq yarn
+npm install -g yarn
 
 # Install global packages
 yarn global add \
