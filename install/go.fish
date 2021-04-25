5b3ddae1c96c30076346ed14d86dd9a02e625e20
Jonah Snider <jonah@jonah.pw>
2021-04-25 01:48:40 -0700

    feat(go): upgrade go to v1.16.3

--- install/go.fish
+++ install/go.fish
@@ -2,7 +2,7 @@
 # Install Go
 
 set filename /tmp/go.tar.gz
-wget -q -O $filename https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
+wget -q -O $filename https://golang.org/dl/go1.16.3.linux-amd64.tar.gz
 
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf $filename
