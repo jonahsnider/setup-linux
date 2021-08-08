e623ba841bf583548f28d5c49bb2ea67dc28444a
Jonah Snider <jonah@jonahsnider.com>
2021-08-08 02:25:50 -0700

    feat(rust): remove quiet flag from `cargo install`

--- install/rust.fish
+++ install/rust.fish
@@ -6,5 +6,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup.sh
 sh /tmp/rustup.sh -yq --no-modify-path
 
 # Starship
-cargo install -q starship fnm
+cargo install starship fnm
 ln -s $SETUP_LINUX_INSTALL_DIR/config/starship/starship.toml ~/.config/starship.toml
