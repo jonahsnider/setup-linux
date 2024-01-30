e8b0185835beb7cb7afc658d729033f3224cc993
Jonah Snider <jonah@jonahsnider.com>
2024-01-29 21:15:22 -0800

    feat: add Proto to PATH

--- config/fish/modules/env.fish
+++ config/fish/modules/env.fish
@@ -14,6 +14,10 @@ set -gx NODE_ENV development
 set -gx DENO_INSTALL ~/.deno
 #endregion
 
+#region Proto
+set -gx PROTO_HOME "$HOME/.proto"
+set -gx PATH "$PROTO_HOME/shims:$PROTO_HOME/bin" $PATH
+#endregion
 
 #region Bun
 set -gx BUN_INSTALL "$HOME/.bun"
